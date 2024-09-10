29539de9051f95b491311770223fe93cca377465
unknown <divyaneela75@gmail.com>
2024-09-10 15:37:15 +0530

    first commit

--- src/pages/File.jsx
+++ src/pages/File.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
+import HindiPDF from '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Hindi.pdf';
+import KannadaPDF from '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Kannada.pdf';
+import MalyalamPDF from '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Malyalam.pdf';
+import TamilPDF from '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Tamil.pdf';
+import TeluguPDF from '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Telugu.pdf';
+import UrduPDF from '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Urdu.pdf';
+import FinancialDoc from '../download/Financial Details Updation Form.docx';
+import GrievancesDoc from '../download/Grievances Redressal Procedure.docx';
+import InvestorsDoc from '../download/Investors attention.docx';
+import ModificationDoc from '../download/Modification form.docx';
+import NominationDoc from '../download/Nomination Form.docx';
+import RightsObligationsDoc from '../download/RIGHTS AND OBLIGATIONS OF STOCK BROKERS.docx';
+import TradingClosureDoc from '../download/Trading Account Closure form.docx';
 
 const File = () => {
   // Define your PDFs with their respective folder paths
-  const pdfs = [
-    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Hindi.pdf', url: '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Hindi.pdf' },
-    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Kannada.pdf', url: '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Kannada.pdf' },
-    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Malyalam.pdf', url: '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Malyalam.pdf' },
-    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Tamil.pdf', url: '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Tamil.pdf' },
-    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Telugu.pdf', url: '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Telugu.pdf' },
-    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Urdu.pdf', url: '../download/CLIENT REGISTRATION ADDITIONAL DOCUMENTS - VERNACULAR LANGUAGES/Urdu.pdf' },
-    { name: 'Financial Details Updation Form.docx', url: '../download/Financial Details Updation Form.docx'},
-    { name: 'Grievances Redressal Procedure.docx', url: '../download/Grievances Redressal Procedure.docx' },
-    { name: 'Investors attention.docx', url: '../download/Investors attention.docx' },
-    { name: 'Modification form.docx', url: '../download/Modification form.docx' },
-    { name: 'Nomination Form.docx', url: '../download/Nomination Form.docx' },
-    { name: 'RIGHTS AND OBLIGATIONS OF STOCK BROKERS.docx', url: '../download/RIGHTS AND OBLIGATIONS OF STOCK BROKERS.docx' },
-    { name: 'Trading Account Closure form.docx', url: '../download/Trading Account Closure form.docx' }
+  const files = [
+    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Hindi.pdf', url: HindiPDF },
+    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Kannada.pdf', url: KannadaPDF },
+    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Malyalam.pdf', url: MalyalamPDF },
+    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Tamil.pdf', url: TamilPDF },
+    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Telugu.pdf', url: TeluguPDF },
+    { name: 'CLIENT REGISTRATION ADDITIONAL DOCUMENTS-Urdu.pdf', url: UrduPDF },
+    { name: 'Financial Details Updation Form.docx', url: FinancialDoc },
+    { name: 'Grievances Redressal Procedure.docx', url: GrievancesDoc },
+    { name: 'Investors attention.docx', url: InvestorsDoc },
+    { name: 'Modification form.docx', url: ModificationDoc },
+    { name: 'Nomination Form.docx', url: NominationDoc },
+    { name: 'RIGHTS AND OBLIGATIONS OF STOCK BROKERS.docx', url: RightsObligationsDoc },
+    { name: 'Trading Account Closure form.docx', url: TradingClosureDoc }
   ];
 
   // Inline styles
@@ -57,12 +70,12 @@ const File = () => {
 
   return (
     <div style={containerStyle} className='mt-5 mb-5'>
-      <h2 style={{ color: '#094E8F', fontWeight: '600', textAlign: 'center' }} className='mb-5'>Available PDFs</h2>
+      <h2 style={{ color: '#094E8F', fontWeight: '600', textAlign: 'center' }} className='mb-5'>Available Files</h2>
       <ul style={listStyle}>
-        {pdfs.map((pdf, index) => (
+        {files.map((file, index) => (
           <li key={index} style={itemStyle}>
-            <a href={pdf.url} download style={linkStyle} onMouseOver={e => e.currentTarget.style.textDecoration = linkHoverStyle.textDecoration} onMouseOut={e => e.currentTarget.style.textDecoration = linkStyle.textDecoration}>
-              {pdf.name}
+            <a href={file.url} download style={linkStyle} onMouseOver={e => e.currentTarget.style.textDecoration = linkHoverStyle.textDecoration} onMouseOut={e => e.currentTarget.style.textDecoration = linkStyle.textDecoration}>
+              {file.name}
             </a>
           </li>
         ))}
