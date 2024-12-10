016800df7cdda8b03e33a7af2c1d230046191576
unknown <divyaneela75@gmail.com>
2024-12-10 11:19:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import VerifyLogin from './components/VerifyLogin';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import DashboardNavbar from './components/DashboardNavbar'; // New Navbar for Dashboard
 import ProfileForm from './components/ProfileForm';
-import ProtectedRoute from './components/ProtectedRoute';
+// import ProtectedRoute from './components/ProtectedRoute';
 import TermsAndCondition from './pages/TermsAndCondition';
 import RefundCancellation from './pages/RefundCancellation';
 
